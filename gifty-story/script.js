e90f38c5528c285ef9204abd2cfda6f9b817695a
unitedpolebs <unitedpolebs@gmail.com>
2026-01-23 21:06:25 +0300

    Update script.js

--- gifty-story/script.js
+++ gifty-story/script.js
@@ -1,10 +1,1 @@
-const elements = document.querySelectorAll(".scroll-animate");
 
-window.addEventListener("scroll", () => {
-  elements.forEach(el => {
-    const position = el.getBoundingClientRect().top;
-    if (position < window.innerHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-});
